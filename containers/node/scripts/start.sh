9fb0edce1b6dfaf9e7345679f5084918e158c7a4
Kun Deng <kundeng@umich.edu>
2020-11-08 23:25:32 -0500

    use alias to simulate fqdn, fixed postgres auth,  todo: move env to image file itself optimized dockerfile by reducing layers change base to centos7 fixed java TLS issue with ambari-agent

--- containers/node/scripts/start.sh
+++ containers/node/scripts/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sed -i "s/hostname=localhost/hostname=ambari-server/" /etc/ambari-agent/conf/ambari-agent.ini
+grep -q '^force_https_protocol' /etc/ambari-agent/conf/ambari-agent.ini && echo "force_https exists!" || sed -i 's/^\[security\]/\[security\]\nforce_https_protocol=PROTOCOL_TLSv1_2/' /etc/ambari-agent/conf/ambari-agent.ini 
 ambari-agent restart
 
+# todo: docker zombie reaping issue. 
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/id_rsa
 chmod 640 /root/.ssh/id_rsa.pub
